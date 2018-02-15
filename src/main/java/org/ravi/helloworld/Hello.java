cb01c973a46c68a15f4ec130534ad4400e22b42c
PDC3A-Training.PDC3A <PDC3A-Training.PDC3A@accenture.com>
2018-02-15 17:06:22 +0530

    changes to helloworld

--- src/main/java/org/ravi/helloworld/Hello.java
+++ src/main/java/org/ravi/helloworld/Hello.java
@@ -3,7 +3,7 @@ package org.ravi.helloworld;
 public class Hello {
 	
 	public String displayHelloMessage(String message){
-		message = "hi....13";
+		message = "hi wats up";
 		return message;
 	}
 
